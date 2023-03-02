0f11cb3f6bb34ff45779dc75b56279171b85cdd1
Melrose Ceballos <melroseceballos@Melroses-MBP.lan>
2023-03-02 11:20:18 -0800

    Following with scott during lecture

--- app.js
+++ app.js
@@ -1,49 +1,62 @@
-const table = document.createElement('table')
-table.innerHTML = `
-        <tr>
-            <th>Name</th>
-            <th>Weight</th>
-            <th>Height</th>
-            <th>Type</th>
-        </tr>
-    `
-// FETCHING API AND TURNING RESULTS TO JSON
-async function fetchPokemon(url) {
+//  Async function that will query an endpoint using fetch and convert the response to a JSON object
+async function getPokemon(url) {
     return await fetch(url)
         .then(res => res.json())
 }
 
-// GOING INTO API INDICES
-fetchPokemon('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+// Get all pokemon's individual endpoints
+getPokemon('https://pokeapi.co/api/v2/pokedex/2/')
     .then(data => {
-        for (let pokemon of data.results) {
-            fetchPokemon(pokemon.url)
-                .then(pokemon => {
-                    const row = document.createElement('tr')
-                    const namePokemon = document.createElement('td')
-                    const weightPokemon = document.createElement('td')
-                    const heightPokemon = document.createElement('td')
-                    const typePokemon = document.createElement('td')
-                    const imgPokemon = document.createElement('td')
-                    namePokemon.innerText = pokemon.name
-                    weightPokemon.innerText = pokemon.weight
-                    heightPokemon.innerText = pokemon.height
-                    typePokemon.innerText = pokemon.types[0].type.name
-                    imgPokemon.innerHTML = `<img src="${pokemon.sprites.front_default}">`
-                    //  IMAGES PATHWAY
-                    console.log(imgPokemon)
-                    
-                    row.append(namePokemon, weightPokemon, heightPokemon,typePokemon,imgPokemon)
-                    table.append(row)
+
+        // Fetch & store data from each pokemon's endpoint
+        const allPokeData = []
+        for (let pokemon of data.pokemon_entries) {
+            const pokemonUrl = pokemon.pokemon_species.url.replace('-species', '')
+            getPokemon(pokemonUrl)
+                .then(res => {
+                    allPokeData.push(res)
+                    const img = document.createElement('img');
+                    img.src = res.sprites.other["official-artwork"].front_default
+                    document.getElementById('carousel-images').append(img)
                 })
+            console.log(allPokeData)
         }
 
-        // Append the finished table to the page
-        document.body.innerHTML = ""
-        document.body.append(table)
+        // KEEP TRACK OF WHICH IMAGE IS SHOWING
+        let currentImgIndex = 0;
+        let previousImgIndex = 0;
+
+
+        // SELECT ALL IMAGE ELEMENTS
+        const images = document.getElementsByTagName('img');
+
+        // CREATE A FUNCTION THAT WILL CYCLE THROUGH THE IMAGES, REGARDLESS OF WHICH BUTTONW AS PRESSED
+        function cycle(nextToPrevious) {
+            // set the index of the previous image to the index of the image that is currently being shown
+            previousImgIndex = currentImgIndex
+            currentImgIndex = currentImgIndex + nextToPrevious
+            // hide the previous image
+            images[previousImgIndex].style.display = 'none';
+
+            // check if the next image index exists in our images array
+            if (currentImgIndex >= images.length) {
+                // if the next image index does not exist, rest the index back to zero
+                currentImgIndex = 0;
+            } else if (currentImgIndex < 0) {
+                // if the next image index does not exist, rest the index back to zero
+                currentImgIndex = images.length + nextToPrevious;
+            }
+            // display the next image
+            images[currentImgIndex].style.display = 'block'
+        }
+
+
+        // ADD EVENT LISTENER TO PREVIOUS BUTTON
+        const prev = document.querySelector('.prev')
+        prev.addEventListener('click', () => cycle(-1))
+
+
+        // ADD EVENT LISTENER TO NEXT BUTTON
+        const next = document.querySelector('.next')
+        next.addEventListener('click', () => cycle(1))
     })
-
-
-    // IMAGE CAROUSEL
-    const pokeImages = document.getElementsByClassName('imgPokemon');
-    
